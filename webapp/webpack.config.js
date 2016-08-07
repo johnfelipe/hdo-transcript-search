d275b42d22aa9031fa8bddbfe95d54a8966603e1
Hendrik <wimpie.crause@gmail.com>
2016-08-07 11:58:43 +0200

    Got dates to display in spanish

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
 
     plugins: [
-        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb$/),
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /es$/),
         new ExtractTextPlugin('bundle.[hash].css'),
         function() {
             this.plugin('done', function(stats) {
