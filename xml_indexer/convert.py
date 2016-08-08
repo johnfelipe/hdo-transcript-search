6cf5719348a20921d77eea944752d14c57733e09
Hendrik <wimpie.crause@gmail.com>
2016-08-08 17:25:13 +0200

    Fixed issue where speeches did not show up on context view

--- xml_indexer/convert.py
+++ xml_indexer/convert.py
@@ -42,6 +42,67 @@ json_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'json')
 json_indexed_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'json_indexed')
 elastic = Elasticsearch()
 
+ES_SETTINGS = {
+    'index': {
+        'analysis': {
+            'analyzer': {
+                'analyzer_standard': {
+                    'tokenizer': "standard",
+                    'filter': ["standard", "lowercase"]
+                },
+                'analyzer_shingle': {
+                    'tokenizer': "standard",
+                    'filter': ["standard", "lowercase", "filter_stop", "filter_shingle"]
+                }
+            },
+            'filter': {
+                'filter_shingle': {
+                    'type': "shingle",
+                    'max_shingle_size': 2,
+                    'min_shingle_size': 2,
+                    'output_unigrams': True
+                },
+                'filter_stop': {
+                    'type': "stop",
+                    'stopwords': "_spanish_"
+                    # enable_position_increments: false
+                }
+            }
+        }
+    }
+}
+
+ES_MAPPINGS = {
+    'speech': {
+        'properties': {
+            'time': {
+                'type':   'date',
+                'format': 'date_time_no_millis'
+            },
+            'text': {
+                'type': 'string',
+                'analyzer': 'analyzer_standard',
+                'fields': {
+                    'shingles': { 'type': "string", 'analyzer': "analyzer_shingle"}
+                }
+            },
+            'name':  { 'type': 'string', 'index': 'not_analyzed' },
+            'party': { 'type': 'string', 'index': 'not_analyzed' },
+            'presidents':  {
+                'properties': {
+                    'name':        { 'type': 'string', 'index': 'not_analyzed' },
+                    'external_id': { 'type': 'string', 'index': 'not_analyzed' }
+                }
+            },
+            'title':       { 'type': 'string',  'index': 'not_analyzed' },
+            'external_id': { 'type': 'string',  'index': 'not_analyzed' },
+            'transcript':  { 'type': 'string',  'index': 'not_analyzed' },
+            'session':     { 'type': 'string',  'index': 'not_analyzed' },
+            'order':       { 'type': 'integer', 'index': 'not_analyzed' }
+        }
+    }
+}
+
 
 def convert_xml_to_json():
     xml_files = os.listdir(xml_path)
@@ -101,7 +162,7 @@ def save_json(session, transcript, order, speaker_id, speaker_name, time, text):
         'external_id': speaker_id,
         'name': speaker_name,
         'party': None,
-        'time': time,
+        'time': time + '-05:00',
         'text': text,
         'title': "Representante"
     }
@@ -133,12 +194,13 @@ def create_all_paths():
 
 if __name__ == '__main__':
 
-    DELETE_INDEXES = 'delete_indexes' in sys.argv
+    RECREATE = 'recreate' in sys.argv
 
-    if DELETE_INDEXES:
-        print "Deleting elasticsearch indexes"
+    if RECREATE:
+        print "Deleting elasticsearch indices"
         elastic.indices.delete(index="hdo-transcripts", ignore=[400, 404])
-    else:
-        create_all_paths()
-        convert_xml_to_json()
-        index_json_on_elasticsearch()
+        elastic.indices.create(index="hdo-transcripts", body={ 'settings': ES_SETTINGS, 'mappings': ES_MAPPINGS })
+
+    create_all_paths()
+    convert_xml_to_json()
+    index_json_on_elasticsearch()
